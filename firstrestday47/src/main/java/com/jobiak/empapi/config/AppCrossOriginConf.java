48661a49653289c989da5bde65cd48f34ba9edd1
Sandeep Kumar <sandeepkumar.adimulam@jobiak.com>
2022-05-17 10:45:37 +0530

    Seventh Project

---
+++ firstrestday47/src/main/java/com/jobiak/empapi/config/AppCrossOriginConf.java
@@ -0,0 +1,10 @@
+package com.jobiak.empapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration //Java Configuration
+public class AppCrossOriginConf implements WebMvcConfigurer{
+
+	public void addCrosMappings(Cors)
+}
